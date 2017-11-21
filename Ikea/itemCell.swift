87a54c92f057a3628fda57d65dc30b4f90c33d60
Justin J Russell <justinjrussell001@gmail.com>
2017-11-21 00:07:58 -0800

    Initial Commit

---
+++ Ikea/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Justin Russell on 9/17/17.
+//  Copyright © 2017 Justin Russell. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+}
